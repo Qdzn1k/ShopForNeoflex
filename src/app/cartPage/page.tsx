20c42a04323c6f4cd221ef896e350213afc7044b
Qdzn1k <minakovdenisa@gmail.com>
2025-04-20 22:50:53 +0300

    Finished the task

--- src/app/cartPage/page.tsx
+++ src/app/cartPage/page.tsx
@@ -29,17 +29,17 @@ export default function CartPage() {
     const updatedCart = cart.map(item =>
       item.id === id ? { ...item, quantity: newQuantity } : item
     );
-    
+
     sessionStorage.setItem('cart', JSON.stringify(updatedCart)); // Обновление sessionStorage
     setCart(updatedCart);
 
-    
+
   };
 
   // Функция для удаления товара из корзины
   const handleRemove = (id: string) => {
     const updatedCart = cart.filter(item => item.id !== id);
-    
+
     sessionStorage.setItem('cart', JSON.stringify(updatedCart)); // Обновление sessionStorage
     setCart(updatedCart);
   };
@@ -62,12 +62,16 @@ export default function CartPage() {
               onRemove={handleRemove} />
           ))}
         </div>
-        <div>
-          {/* Здесь может быть информация о стоимости заказа или дополнительные элементы */}
-          <h2 className="font-semibold text-lg">Итоговая стоимость:</h2>
-          <p>
-            {cart.reduce((total, item) => total + item.price * item.quantity, 0)} ₽
-          </p>
+        <div className='ms-[127px] bg-white rounded-[30px]  pt-[21px]  flex flex-col max-h-[114px]'>
+          <div className='flex justify-between items-center ps-[21px] pe-[16px]'>
+            <h2 className="font-semibold text-lg">ИТОГО:</h2>
+            <p className='font-semibold text-[15px]'>
+              ₽ {cart.reduce((total, item) => total + item.price * item.quantity, 0)}
+            </p>
+          </div>
+          <button className='cursor-pointer mt-[15px] bg-[#101010] w-full text-white text-[17px] font-semibold px-[13px] py-[22px] rounded-[30px]'>
+            Перейти к оформлению 
+          </button>
         </div>
       </div>
 
--- src/components/CardInCart.tsx
+++ src/components/CardInCart.tsx
@@ -32,24 +32,46 @@ const CartItem: React.FC<CartItemProps> = ({ item, onQuantityChange, onRemove })
     return (
         <div className="font-semibold bg-white rounded-[30px] px-[18px] pt-[18px] pb-[12px]">
             <div className='flex items-stretch h-full'>
-                <div className='flex items-center justify-end'>
-                    <Image
-                        src={item.image}
-                        alt="Изображение товара"
-                        width={144}
-                        height={144}
-                    />
+                <div className='flex justify-between items-center'>
+
+                    <div className='flex flex-col items-center w-[147px]'>
+                        <Image className=''
+                            src={item.image}
+                            alt="Изображение товара"
+                            width={144}
+                            height={144}
+                        />
+                        <div className='mt-[20px] flex items-center justify-between'>
+                            <div className="flex items-center">
+                                <button
+                                    onClick={handleDecrement}
+                                    className="bg-[#FFCE7F] px-2 rounded-full text-white cursor-pointer"
+                                >
+                                    −
+                                </button>
+                                <span className='w-10 text-center text-[#1C1C27]'>{item.quantity}</span>
+                                <button
+                                    onClick={handleIncrement}
+                                    className="bg-[#FFCE7F] px-2 rounded-full text-white cursor-pointer"
+                                >
+                                    +
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
                     <div className='ms-[23px]'>
                         <p className="font-medium">{item.name}</p>
                         <p className="mt-[12px] text-sm text-gray-500">{item.price} ₽</p>
                     </div>
+
                 </div>
 
                 <div className='flex flex-col justify-between items-end ml-auto'>
-                    <button className='w-[20px]'>
+                    <button className='w-[20px] cursor-pointer' onClick={() => onRemove(item.id)}>
                         <Image
                             src='/Bucket.svg'
-                            alt="Изображение товара"
+                            alt=""
                             width={20}
                             height={20}
                         />
@@ -60,29 +82,7 @@ const CartItem: React.FC<CartItemProps> = ({ item, onQuantityChange, onRemove })
                 </div>
             </div>
 
-            <div className='ms-6 flex items-center justify-between'>
-                <div className="flex items-center">
-                    <button
-                        onClick={handleDecrement}
-                        className="bg-[#FFCE7F] px-2 rounded-full text-white"
-                    >
-                        −
-                    </button>
-                    <span className='w-10 text-center text-[#1C1C27]'>{item.quantity}</span>
-                    <button
-                        onClick={handleIncrement}
-                        className="bg-[#FFCE7F] px-2 rounded-full text-white"
-                    >
-                        +
-                    </button>
-                </div>
-                <button
-                    onClick={() => onRemove(item.id)}
-                    className="bg-red-500 px-4 py-2 text-white rounded-md"
-                >
-                    Удалить
-                </button>
-            </div>
+
         </div>
     );
 };
